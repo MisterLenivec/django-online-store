3de743a75d170934e9663597a9e35af662797508
lenivec <wormsom@gmail.com>
2019-09-22 04:11:52 +0300

    Reverting to the state of the project at a064f8bef829328a78eb92f1ca5ce15e2fda6427

--- orders/views.py
+++ orders/views.py
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.shortcuts import render, redirect, get_object_or_404
-=======
-from django.shortcuts import render, redirect
->>>>>>> master
-=======
-from django.shortcuts import render, redirect
->>>>>>> b88cb93ce4f3c7485da838f41cca81a70688b995
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.core.mail import get_connection, EmailMultiAlternatives
 from django.urls import reverse
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.contrib.admin.views.decorators import staff_member_required
-=======
->>>>>>> master
-=======
->>>>>>> b88cb93ce4f3c7485da838f41cca81a70688b995
 
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
@@ -42,10 +28,6 @@ def order_create(request):
             context.update({'cart': cart, 'order': order})
             request.session['order_id'] = order.id
             return redirect(reverse('payment:process'))
-<<<<<<< HEAD
-=======
-            # return render(request, 'orders/order/created.html', {'order': order})
->>>>>>> b88cb93ce4f3c7485da838f41cca81a70688b995
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart,
@@ -67,15 +49,9 @@ def order_created():
 
     connection.close()  # Cleanup
     return mail_send
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
 @staff_member_required
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'admin/orders/order/detail.html', {'order': order})
-=======
->>>>>>> master
-=======
->>>>>>> b88cb93ce4f3c7485da838f41cca81a70688b995
