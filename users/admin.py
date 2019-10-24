f82c663cb23fe2d50869564115650a63274b4365
lenivec <wormsom@gmail.com>
2019-10-25 00:43:41 +0300

    add editable account pfofile

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from .models import Profile
 
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'username', 'email',
-#                   'phone_number', 'password1', 'password2')
-#     fields = ['first_name', 'last_name', 'username', 'email',
-#                   'phone_number', 'password1', 'password2']
-
-
+admin.site.register(Profile)
--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,27 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
-from phonenumber_field.formfields import PhoneNumberField
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    phone_number = PhoneNumberField()
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email',
-                  'phone_number', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1',
+                  'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone', 'image']
