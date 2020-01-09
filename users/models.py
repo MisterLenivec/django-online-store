3df2a94fb81d09ea52fbc780309f5725cbe05469
lenivec <wormsom@gmail.com>
2020-01-09 15:23:47 +0300

    Changed the method in users model, changed template, added the settings folder

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import io
+from django.core.files.storage import default_storage as storage
 
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -20,12 +22,19 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self,  *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img_read = storage.open(self.image.name, 'r')
+        img = Image.open(img_read)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            in_mem_file = io.BytesIO()
+            img.convert('RGB').save(in_mem_file, format='JPEG')
+            img_write = storage.open(self.image.name, 'w+')
+            img_write.write(in_mem_file.getvalue())
+            img_write.close()
+
+        img_read.close()
